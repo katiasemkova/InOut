cbd6580d801399c9035425121190c08bb846ee57
katiasemkova <katia_semkova@abv.bg>
2021-09-18 22:17:16 +0300

    Add project files.

---
+++ InOut/Controllers/ItemController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InOut.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ItemsExpensesDbContext _db;
+        public ItemController(ItemsExpensesDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> obj = _db.Items;
+            return View(obj);
+        }
+        //Create Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item item)
+        {
+           
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+      //Create Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Update GET
+        public IActionResult Update(int?id)
+        {
+            var obj = _db.Items.Find(id);
+           
+
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Item obj)
+        {
+            if(obj==null)
+            {
+                return NotFound();
+            }
+            _db.Items.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        //Get Delete
+        public IActionResult Delete(int? id)
+        {
+            var obj = _db.Items.Find(id);
+           
+            return View(obj);
+        }
+        //Post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Items.Find(id);
+         
+            _db.Items.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
---
+++ InOut/Migrations/20210829092032_NewInOut.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InOut.Migrations
+{
+    public partial class NewInOut : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExpenseName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExpenseTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExpenseTypeName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Borrower = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Lender = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expenses");
+
+            migrationBuilder.DropTable(
+                name: "ExpenseTypes");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
